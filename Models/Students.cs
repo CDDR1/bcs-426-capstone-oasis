38ef1fab18544411356273a8162a1f0eee2f5074
Alex Monteverde <55471655+Amonteverde04@users.noreply.github.com>
2022-05-14 14:17:35 -0400

    Controller and Model
    - Controller and model for students table
    
    Co-Authored-By: Carlos Duque <62437724+CDDR1@users.noreply.github.com>
    Co-Authored-By: Gresba <79733876+Gresba@users.noreply.github.com>

---
+++ Models/Students.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnhancedOasis.Models
+{
+    public class Students
+    {
+        public int StudentID { get; set; }
+        public string Email { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Password { get; set; }
+        public string Phone_Number { get; set; }
+        public string Major { get; set; }
+    }
+}
